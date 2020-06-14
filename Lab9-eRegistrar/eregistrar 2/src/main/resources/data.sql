2e3ed461a5e6131435d9f51c8d9f442bd462e226
eyob05 <65434701+eyob05@users.noreply.github.com>
2020-06-13 20:02:07 -0500

    committed

---
+++ Lab9-eRegistrar/eregistrar 2/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into students(cgpa ,enrollment_date ,first_name,last_name,middle_name,student_number,is_international)
+values (3.4,'2020-02-18','Eyob','Weldeyohannes','Habtom','12346',"yes");
+
+insert into students(cgpa ,enrollment_date ,first_name,last_name,middle_name,student_number,is_international)
+values (3.4,'2020-02-08','Selam','Ghide','Esset','12345',"no");
