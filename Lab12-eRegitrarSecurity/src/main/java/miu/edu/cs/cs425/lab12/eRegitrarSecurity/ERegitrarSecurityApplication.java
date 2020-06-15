201b15337e6a78d571448592c29305fb7055d214
eyob05 <65434701+eyob05@users.noreply.github.com>
2020-06-14 23:36:28 -0500

    committed

---
+++ Lab12-eRegitrarSecurity/src/main/java/miu/edu/cs/cs425/lab12/eRegitrarSecurity/ERegitrarSecurityApplication.java
@@ -0,0 +1,13 @@
+package miu.edu.cs.cs425.lab12.eRegitrarSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+//@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+public class ERegitrarSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ERegitrarSecurityApplication.class, args);
+	}
+
+}
